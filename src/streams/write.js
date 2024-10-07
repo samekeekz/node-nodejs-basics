446cfe47e5f6e1b9f71628e239b83045d89c14f4
samatbelentbay <belentbais03@mail.ru>
2024-10-08 00:13:51 +0500

    feat: add streams functions

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,26 @@
+import { createWriteStream } from 'fs';
+import path from 'path';
+
+const moduleURL = new URL(import.meta.url);
+const sourceFolder = path.join(path.dirname(moduleURL.pathname), 'files');
+const filePath = path.join(sourceFolder, 'fileToWrite.txt');
+
+
 const write = async () => {
-    // Write your code here 
+    return new Promise((resolve, reject) => {
+        const stream = createWriteStream(filePath, { encoding: 'utf-8' });
+
+        process.stdin.pipe(stream);
+
+        stream.on('finish', () => {
+            resolve();
+        });
+
+        stream.on('error', (error) => {
+            reject(error);
+        });
+    });
 };
+
 
 await write();
