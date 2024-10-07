92fa245f60d6999229b50f2d9ba54f01bf62958c
samatbelentbay <belentbais03@mail.ru>
2024-10-08 00:13:09 +0500

    feat: add cp function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import path from 'path';
+import { spawn } from 'child_process';
+
+const moduleURL = new URL(import.meta.url);
+const sourceFolder = path.join(path.dirname(moduleURL.pathname), 'files');
+const filePath = path.join(sourceFolder, "script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = spawn('node', [filePath, ...args], { stdio: ['pipe', 'pipe', 'inherit', 'ipc'] });
+
+    process.stdin.pipe(childProcess.stdin);
+
+    childProcess.stdout.pipe(process.stdout);
+
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 3, "a", "b", "hello"]);
