a772c5952671ab1a83c396e0468cee8a666970b0
samatbelentbay <belentbais03@mail.ru>
2024-10-08 00:14:00 +0500

    feat: add hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,31 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import path from 'path';
+
+const moduleURL = new URL(import.meta.url);
+const sourceFolder = path.join(path.dirname(moduleURL.pathname), 'files');
+const filePath = path.join(sourceFolder, 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    return new Promise((resolve, reject) => {
+        const hash = createHash('sha256');
+        const stream = createReadStream(filePath);
+
+        stream.on('data', (chunk) => {
+            hash.update(chunk);
+        });
+
+        stream.on('end', () => {
+            const hashResult = hash.digest('hex');
+            console.log(`${hashResult}`);
+            resolve(hashResult);
+        });
+
+        stream.on('error', (error) => {
+            console.error(`Error reading file: ${error.message}`);
+            reject(error);
+        });
+    });
 };
 
 await calculateHash();
